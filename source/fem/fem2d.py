c4f2b1e25471d4d0bfcf07883752e6ea9d9680d9
wprzadka <wiktor.przadka.1@gmail.com>
2022-06-04 15:33:16 +0200

    refactor code structure

--- source/fem/fem2d.py
+++ source/fem/fem2d.py
@@ -1,14 +1,18 @@
 from typing import Optional
 import numpy as np
 
+from source.mesh import Mesh
+
 
 class FEM:
     def __init__(
             self,
+            mesh: Mesh,
             rhs_func: callable,
             dirichlet_func: Optional[callable],
             neumann_func: Optional[callable]
     ):
+        self.mesh = mesh
         self.rhs_func = rhs_func
         self.dirichlet_func = dirichlet_func
         self.neumann_func = neumann_func
@@ -30,3 +34,41 @@ class FEM:
         # vertices = [[x0, y0], [x1, y1]]
         values = np.array([self.dirichlet_func(v) for v in vertices])
         return np.expand_dims(values, 1)
+
+    def solve(self):
+        nodes_num = self.mesh.coordinates2D.shape[0]
+        A = np.zeros(shape=(nodes_num, nodes_num))
+        b = np.zeros(shape=(nodes_num,))
+
+        # Assembly stiffness matrix
+        for elem in self.mesh.nodes_of_elem:
+            local = self.stima3(self.mesh.coordinates2D[elem])
+            for y, col in enumerate(elem):
+                for x, row in enumerate(elem):
+                    A[row, col] += local[x, y]
+
+        # assembly rhs vector
+        for elem in self.mesh.nodes_of_elem:
+            rhs = self.rhs_val(self.mesh.coordinates2D[elem])
+            b[elem] += rhs
+
+        # neumann conditions
+        for vert_idxs in self.mesh.neumann_boundaries:
+            b[vert_idxs] += self.neumann_val(self.mesh.coordinates2D[vert_idxs])
+
+        # dirichlet conditions
+        u = np.zeros(shape=(nodes_num, 1))
+        for vert_idxs in self.mesh.dirichlet_boundaries:
+            u[vert_idxs] = self.dirichlet_val(self.mesh.coordinates2D[vert_idxs])
+        b -= (A @ u).T[0]
+
+        free_nodes = [v for v in range(nodes_num) if v not in self.mesh.dirichlet_boundaries]
+
+        # A = A[free_nodes][:, free_nodes]
+        # b = b[free_nodes]
+
+        # solve
+        u_free = np.linalg.solve(A[free_nodes][:, free_nodes], b[free_nodes])
+        w = np.squeeze(u.copy(), 1)
+        w[free_nodes] = u_free
+        return w
--- source/main.py
+++ source/main.py
@@ -7,7 +7,28 @@ from matplotlib import tri
 from fem.laplace_setup import LaplaceSetup
 
 
+def plot_exact_solution(exact: callable):
+    X, Y = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
+    exact_values = np.reshape(exact(np.stack((X, Y))), X.shape)
+    plt.contour(X, Y, exact_values)
+    plt.savefig('exact.png')
+    plt.close()
+
+
+def plot_results(results: np.ndarray):
+    triangulation = tri.Triangulation(
+        x=mesh.coordinates2D[:, 0],
+        y=mesh.coordinates2D[:, 1],
+        triangles=mesh.nodes_of_elem
+    )
+    plt.tricontourf(triangulation, results)
+    plt.colorbar()
+    plt.savefig('results.png')
+    plt.close()
+
+
 if __name__ == '__main__':
+
     mesh = Mesh('meshes/rectangle.msh')
 
     rhs_func = lambda x: -np.sin(x[0] * np.pi) * np.sin(x[1] * np.pi)
@@ -16,66 +37,18 @@ if __name__ == '__main__':
     neumann_func = lambda x: 0
 
     fem = LaplaceSetup(
+        mesh=mesh,
         rhs_func=rhs_func,
         dirichlet_func=dirichlet_func,
         neumann_func=neumann_func
     )
-
-    nodes_num = mesh.coordinates2D.shape[0]
-    A = np.zeros(shape=(nodes_num, nodes_num))
-    b = np.zeros(shape=(nodes_num,))
-
-    # Assembly stiffness matrix
-    for elem in mesh.nodes_of_elem:
-        local = fem.stima3(mesh.coordinates2D[elem])
-        for y, col in enumerate(elem):
-            for x, row in enumerate(elem):
-                A[row, col] += local[x, y]
-
-    # assembly rhs vector
-    for elem in mesh.nodes_of_elem:
-        rhs = fem.rhs_val(mesh.coordinates2D[elem])
-        b[elem] += rhs
-
-    # neumann conditions
-    for vert_idxs in mesh.neumann_boundaries:
-        b[vert_idxs] += fem.neumann_val(mesh.coordinates2D[vert_idxs])
-
-    # dirichlet conditions
-    u = np.zeros(shape=(nodes_num, 1))
-    for vert_idxs in mesh.dirichlet_boundaries:
-        u[vert_idxs] = fem.dirichlet_val(mesh.coordinates2D[vert_idxs])
-    b -= (A @ u).T[0]
-
-    free_nodes = [v for v in range(nodes_num) if v not in mesh.dirichlet_boundaries]
-
-    # A = A[free_nodes][:, free_nodes]
-    # b = b[free_nodes]
-
-    # solve
-    u_free = np.linalg.solve(A[free_nodes][:, free_nodes], b[free_nodes])
-    w = np.squeeze(u.copy(), 1)
-    w[free_nodes] = u_free
-
+    results = fem.solve()
     print("solution")
-    print(w)
+    print(results)
 
     # plot results of FEM
-    triangulation = tri.Triangulation(
-        x=mesh.coordinates2D[:, 0],
-        y=mesh.coordinates2D[:, 1],
-        triangles=mesh.nodes_of_elem
-    )
-    plt.tricontourf(triangulation, w)
-    plt.colorbar()
-    plt.savefig('results.png')
-    plt.close()
+    plot_results(results)
 
     # validation
     exact = lambda x: -np.sin(x[0] * np.pi) * np.sin(x[1] * np.pi) / (2 * np.pi ** 2)
-
-    X, Y = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
-    exact_values = np.reshape(exact(np.stack((X, Y))), X.shape)
-    # plt.close()
-    plt.contour(X, Y, exact_values)
-    plt.savefig('exact.png')
+    plot_exact_solution(exact)
