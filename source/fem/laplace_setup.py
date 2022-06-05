2188a184790d6b66948173233383ab6bcb312421
wprzadka <wiktor.przadka.1@gmail.com>
2022-06-05 12:57:22 +0200

    extract some generic functions to utils

--- source/fem/laplace_setup.py
+++ source/fem/laplace_setup.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+from source.fem import utils
 from source.fem.fem2d import FEM
 
 
@@ -6,13 +8,12 @@ class LaplaceSetup(FEM):
 
     def stima3(self, vertices: np.ndarray):
         # cords = np.array([['x1', 'y1'], ['x2', 'y2'], ['x3', 'y3']])
-        doubleT = vertices[1:3].T - np.expand_dims(vertices[0], 1)
-        detT = np.linalg.det(doubleT) / 2
+        T = utils.area_of_triangle(vertices)
         G_b = np.vstack((
             np.ones(3),
             vertices[:, 0],
             vertices[:, 1]
         ))
         G = np.linalg.inv(G_b) @ np.array([[0, 0], [1, 0], [0, 1]])
-        M = (detT / 2) * (G @ G.T)
+        M = (T / 2) * (G @ G.T)
         return M
---
+++ source/fem/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def area_of_triangle(coords: np.ndarray) -> float:
+    # coords = np.array([['x1', 'y1'], ['x2', 'y2'], ['x3', 'y3']])
+    doubleT = coords[1:3].T - np.expand_dims(coords[0], 1)
+    areaT = np.abs(np.linalg.det(doubleT)) / 2
+    return areaT
