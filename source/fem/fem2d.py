e840d04d35465344bb21a55ad4bf3c0327951440
wprzadka <wiktor.przadka.1@gmail.com>
2022-07-27 12:57:21 +0200

    reformat code structure

--- source/fem/fem2d.py
+++ source/fem/fem2d.py
@@ -38,40 +38,4 @@ class FEM:
         return values
 
     def solve(self):
-        nodes_num = self.mesh.coordinates2D.shape[0]
-        A = np.zeros(shape=(nodes_num, nodes_num))
-        b = np.zeros(shape=(nodes_num,))
-
-        # Assembly stiffness matrix
-        for elem in self.mesh.nodes_of_elem:
-            local = self.stima3(self.mesh.coordinates2D[elem])
-            for y, col in enumerate(elem):
-                for x, row in enumerate(elem):
-                    A[row, col] += local[x, y]
-
-        # assembly rhs vector
-        for elem in self.mesh.nodes_of_elem:
-            rhs = self.rhs_val(self.mesh.coordinates2D[elem])
-            b[elem] += rhs
-
-        # neumann conditions
-        for vert_idxs in self.mesh.neumann_boundaries:
-            b[vert_idxs] += self.neumann_val(self.mesh.coordinates2D[vert_idxs])
-
-        # dirichlet conditions
-        u = np.zeros(shape=(nodes_num, 1))
-        for vert_idxs in self.mesh.dirichlet_boundaries:
-            values = self.dirichlet_val(self.mesh.coordinates2D[vert_idxs])
-            u[vert_idxs] = np.expand_dims(values, 1)
-        b -= (A @ u).T[0]
-
-        free_nodes = [v for v in range(nodes_num) if v not in self.mesh.dirichlet_boundaries]
-
-        # A = A[free_nodes][:, free_nodes]
-        # b = b[free_nodes]
-
-        # solve
-        u_free = np.linalg.solve(A[free_nodes][:, free_nodes], b[free_nodes])
-        w = np.squeeze(u.copy(), 1)
-        w[free_nodes] = u_free
-        return w
+        raise NotImplementedError
