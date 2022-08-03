66d7baf94cd0d94c26cd2afc5db3ec34d6ca8cef
wprzadka <wiktor.przadka.1@gmail.com>
2022-08-03 15:14:43 +0200

    move base functions to utils

--- source/fem/utils.py
+++ source/fem/utils.py
@@ -10,3 +10,17 @@ def area_of_triangle(coords: np.ndarray) -> float:
 
 def center_of_mass(coords: np.ndarray) -> np.ndarray:
     return np.sum(coords, 0) / coords.shape[0]
+
+
+def base_func(x: np.ndarray, vertices: np.ndarray):
+    base = np.concatenate((x.T, vertices[0:2]), 0)
+    numerator = np.concatenate((np.ones((3, 1)), base), 1)
+    denominator = np.concatenate((np.ones((3, 1)), vertices), 1)
+    return np.linalg.det(numerator) / np.linalg.det(denominator)
+
+
+def grad_base_func(vertices: np.ndarray, idx: int):
+    i = (idx + 1) % 3
+    j = (idx + 2) % 3
+    return np.array([vertices[i, 1] - vertices[j, 1], vertices[j, 0] - vertices[i, 0]]) \
+           / (2 * area_of_triangle(vertices))
--- tests/fem2d_tests.py
+++ tests/fem2d_tests.py
@@ -4,19 +4,13 @@ import numpy as np
 import quadpy
 import pytest
 
+from source.fem import utils
 from source.fem.fem2d import FEM
 
 
 class MeshMock:
     dirichlet_boundaries = np.array([[]])
     neumann_boundaries = np.array([[]])
-
-
-def base_func(x: np.ndarray, vertices: np.ndarray):
-    base = np.concatenate((x.T, vertices[0:2]), 0)
-    numerator = np.concatenate((np.ones((3, 1)), base), 1)
-    denominator = np.concatenate((np.ones((3, 1)), vertices), 1)
-    return np.linalg.det(numerator) / np.linalg.det(denominator)
 
 
 @pytest.mark.parametrize('coords, func', [
@@ -27,7 +21,7 @@ def base_func(x: np.ndarray, vertices: np.ndarray):
     (np.array([[100, 0], [10, 20], [50, 20]]), lambda x: np.exp(x[0]) + np.exp(x[1]))
 ])
 def test_rhs(coords: np.ndarray, func: Callable[[np.ndarray], float]):
-    wrap_func = lambda x: np.array([func(x) * base_func(x, coords)])
+    wrap_func = lambda x: np.array([func(x) * utils.base_func(x, coords)])
     scheme = quadpy.t2.get_good_scheme(1)
     expected = np.squeeze(scheme.integrate(wrap_func, coords))
     fem = FEM(
