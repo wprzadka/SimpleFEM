e1e18d407dee644a9ce325b8a51d82f4d7952a51
wprzadka <wiktor.przadka.1@gmail.com>
2023-07-18 20:13:02 +0200

    improve the way of level set function initialization

--- source/examples/materials.py
+++ source/examples/materials.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class MaterialProperty(Enum):
+    TestMaterial = (1, 0.3)
     CarbonSteel = (200e9, 0.28)
     Gold = (78e9, 0.44)
     Diamond = (1035e9, 0.29)  # Poisson's ratio = 0.1-0.29
